bf9175a42bf2696b1297a834d9b23f3cb905eecf
Darya Javadi <darya.javadi15@gmail.com>
2024-10-21 07:04:18 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,6 @@
+/*
+ *include
+ */
 #include <stdio.h>
 /**
  *main
@@ -5,5 +8,5 @@
 int main(void) 
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
