704a41abb48c2a551ef7e90aea9c6b15e66f52ad
Darya Javadi <darya.javadi15@gmail.com>
2024-10-22 11:54:26 +0000

    4

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,4 +5,19 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int main(void)
+{
+  int r;
+
+  r = _isalpha('H');
+  _putchar(r + '0');
+  r = _isalpha('o');
+  _putchar(r + '0');
+  r = _isalpha(108);
+  _putchar(r + '0');
+  r = _isalpha(';');
+  _putchar(r + '0');
+  _putchar('\n');
+  return (0);
+}
 #endif /* MAIN_H */
