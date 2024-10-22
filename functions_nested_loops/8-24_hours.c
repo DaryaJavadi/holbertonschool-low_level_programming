2675f07aea19fe2c884ac80030475325c0b4b707
Darya Javadi <darya.javadi15@gmail.com>
2024-10-22 13:21:29 +0000

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0';
+			_putchar((hr % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0';
