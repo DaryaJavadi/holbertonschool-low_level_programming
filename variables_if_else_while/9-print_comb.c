6e17c721ac289b18c6d7322de3ac0e7ba69bc7d3
Darya Javadi <darya.javadi15@gmail.com>
2024-10-22 09:28:07 +0000

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,19 +11,17 @@
 int main(void)
 {
 	int num;
-	char p;
-	char space;
 
 	for (num = '0'; num <= '9'; num ++)
 	{
 		putchar(num);
 		if (num != '9')
 		{
-			putchar (',')
-			putchar (' ')
+			putchar (',');
+			putchar (' ');
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
 
