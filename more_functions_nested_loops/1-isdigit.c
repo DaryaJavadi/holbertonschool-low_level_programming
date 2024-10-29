61881e9370cf39c90e7a1fa7a3c3c161321ae801
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 07:24:56 +0000

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+ *_isdigit-checks if the c is digit
+ *@c: input for numbers
+ *Return: 1 if digit, 0 if not
+ */
+#include <stdio.h>
+int _isdigit(int c)
+{
+	if (c >= 1 && c <= 9)
+	{
+		return 1
+	}
+	return 0
+}
