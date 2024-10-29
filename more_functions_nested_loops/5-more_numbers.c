6e06e388483c11cc67c6de64535493e1f3aedad3
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 10:55:28 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+/**
+*print_most_numbers - Prints the numbers from 0 to 9,
+*                      skipping 2 and 4, followed by a new line.
+*Return: Always 0
+*/
+#include <stdio.h>
+#include"main.h"
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i <= 10; i ++)
+	{
+		for (j = 0; j <= 14; j ++)
+		{
+			_putchar(j + '0')
+		}
+		_putchar('\n')
+	}
+}
