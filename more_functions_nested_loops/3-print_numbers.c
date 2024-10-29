e567cc3bf86ccdae9851c06bbf4b1f7f99e1d88a
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 08:04:23 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+/**
+ *print_numbers-print numbers from 0 to 9
+ *Return: Always 0
+ */
+#include <stdio.h>
+void print_numbers(void)
+{
+	for (int i = 0; i < 100; i++) {
+		_putchar (i)
+	}
+	_putchar('\n')
+}
