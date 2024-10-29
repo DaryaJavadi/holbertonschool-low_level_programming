ac5e900e93fad702fe837568778545d314c07691
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 09:10:26 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,6 @@
 /**
  *print_most_numbers numbers from 0 to 9
+ *@i:input
  *Return: Always 0
  */
 #include <stdio.h>
@@ -7,6 +8,7 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++) 
 	{
 		if (i != 2 && i != 4)
@@ -14,5 +16,5 @@ void print_most_numbers(void)
 		_putchar(i + '0');
 		}
 	}
-    _putchar('\n');
+	_putchar('\n');
 }
