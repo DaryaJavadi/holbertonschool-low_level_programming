72c23480622232a7efd4f289d798e1217a1edf4e
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 12:25:22 +0000

    7

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+/**
+*print_square - Prints the lines,
+*                      n times, followed by a new line.
+*@n: n times
+*Return: Always 0
+*/
+#include <stdio.h>
+#include"main.h"
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
