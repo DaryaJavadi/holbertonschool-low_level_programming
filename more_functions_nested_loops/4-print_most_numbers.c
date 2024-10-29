6c5c521e6e8760689339db7a8d784fcfbfc5cd46
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 09:04:16 +0000

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,12 @@
  *print_most_numbers numbers from 0 to 9
  *Return: Always 0
  */
+#include <stdio.h>
+#include"main.h"
 void print_most_numbers(void)
 {
 	int i;
-	for (int i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++) 
 	{
 		if (i != 2 && i != 4)
 		{
