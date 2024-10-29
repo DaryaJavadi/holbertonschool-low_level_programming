e099c8077905adcf8293467a082b234c77195ab0
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 11:41:02 +0000

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+/**
+*print_line - Prints the lines,
+*                      n times, followed by a new line.
+*Return: Always 0
+*/
+#include <stdio.h>
+#include"main.h"
+void print_line(int n)
+{
+	if n <= 0
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_')
+		}
+		_putchar('\n')
+	}
+}
