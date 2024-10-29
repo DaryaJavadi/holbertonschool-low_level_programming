63844ab97a7df458a7fa453c576d290b8bf7f9f6
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 08:59:06 +0000

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+/**
+ *print_most_numbers numbers from 0 to 9
+ *Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	for (int i = 0; i < 10; i++) 
+	{
+		if (i != 2 && i != 4)
+		{
+		_putchar(i + '0');
+		}
+	}
+    _putchar('\n');
+}
