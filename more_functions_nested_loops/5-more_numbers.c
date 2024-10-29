3a87381b0f84c53d7564eef5f99b903ecc0c4933
Darya Javadi <darya.javadi15@gmail.com>
2024-10-29 11:26:50 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@
 #include"main.h"
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i ++)
 	{
@@ -15,9 +15,9 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar((j/10) + '0');
+				_putchar((j / 10)+'0');
 			}
-			_putchar((j % 10) + '0');
+			_putchar((j % 10)+'0');
 		}
 		_putchar('\n');
 	}
