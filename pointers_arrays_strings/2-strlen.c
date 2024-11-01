89305482c7f3efa6200757f565029bc49d866593
Darya Javadi <darya.javadi15@gmail.com>
2024-11-01 10:47:09 +0000

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,11 @@
 /**
- *swap_int - updates the value to 98
- *@a: input
- *@b: input
+ *strlen - calculate the len of string
+ *@s: string
  *
  */
 #include <stdio.h>
 #include "main.h"
 int _strlen(char *s)
 {
-char str[];
-printf("write your string: %c\n,str")
-int len = strlen(str);
-printf("Length of the string is: %d\n", len)
-	return 0;
+	return strlen(s);
 }
