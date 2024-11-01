b15b101a7c2949b4c5fa7cd03a14978fd406934c
Darya Javadi <darya.javadi15@gmail.com>
2024-11-01 10:42:59 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ *swap_int - updates the value to 98
+ *@a: input
+ *@b: input
+ *
+ */
+#include <stdio.h>
+#include "main.h"
+int _strlen(char *s)
+{
+char str[];
+printf("write your string: %c\n,str")
+int len = strlen(str);
+printf("Length of the string is: %d\n", len)
+	return 0;
+}
