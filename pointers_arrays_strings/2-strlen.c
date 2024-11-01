a5e20d1a0c4631a2eb15ce32c0cb3c114ac75640
Darya Javadi <darya.javadi15@gmail.com>
2024-11-01 11:40:15 +0000

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 /**
  *strlen - calculate the len of string
  *@s: string
- *
+ *Return: len
  */
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
 int _strlen(char *s)
 {
-	int len;
-	
+	int len;	
 	for (len = 0; *s != '\0'; s++)
 		len++;
-	return(len);
+	return (len);
 }
