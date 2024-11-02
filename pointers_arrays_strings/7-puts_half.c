b501f7c6faca0b41857f1fa30a62234f8c804cbc
Darya Javadi <darya.javadi15@gmail.com>
2024-11-02 13:36:31 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+/**
+ * puts_half - prints half of string
+ * @str: input string
+ */
+#include <unistd.h>
+#include "main.h"
+void puts_half(char *str)
+{
+	int length = 0;
+	int i, start;
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+	if (length % 2 == 0)
+	{
+	start = length / 2;
+	}
+	else
+	{
+	start = (length - 1) / 2 + 1;
+	}
+
+	for (i = start; i < length; i++)
+	{
+	write(1, &str[i], 1);
+	}
+	write(1, "\n", 1);
+}
