fe15c15cf5d3808ea47b53a6ed82a6ceb2eda155
Darya Javadi <darya.javadi15@gmail.com>
2024-11-02 14:41:28 +0000

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-	if (i < n && src[i] != '\0')
+	for (i = 0;i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	else
-	{
-		dest[i] = '\0';
-	}
-	}
+	for ( ; i < n; i++)
+	dest[i] = '\0';
+
 	return (dest);
 }
