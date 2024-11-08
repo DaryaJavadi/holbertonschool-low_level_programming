c2de05a50d37672adf5824e4191aee9ef987584f
Darya Javadi <darya.javadi15@gmail.com>
2024-11-08 20:21:16 +0000

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,18 +1,4 @@
-/**
- * _sqrt_recursion - returns the square root of a number
- * @n: number
- * Return: square root of given number
- */
 #include "main.h"
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return find_sqrt(n, 0);
-}
-
 /**
  * sqrt_checker - finds square root
  * @n: given number to find square root
@@ -21,13 +7,33 @@ int _sqrt_recursion(int n)
  */
 int find_sqrt(int n, int i)
 {
-	if (i * i == n)
+        if (i * i == n)
+        {
+                return (i);
+        }
+        else if (i * i > n)
+        {
+                return (-1);
+        }
+	else
 	{
-		return (i);
+        return (find_sqrt(n, i + 1));
 	}
-	else if (i * i > n)
+}
+
+/**
+ * _sqrt_recursion - returns the square root of a number
+ * @n: number
+ * Return: square root of given number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 	{
 		return (-1);
 	}
-	return find_sqrt(n, i + 1);
+	else
+	{
+	return (find_sqrt(n, 0));
+	}
 }
