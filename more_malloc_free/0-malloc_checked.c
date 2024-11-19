b608f6c8bfbbb13fb8867ff7c5f425d577eef589
Darya Javadi <darya.javadi15@gmail.com>
2024-11-19 10:28:31 +0000

    malloc2

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+/**
+ * malloc_checked - allocates memory
+ * @b: size
+ * Return: pointer
+ */
+#include <stdlib.h>
+#include "main.h"
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
