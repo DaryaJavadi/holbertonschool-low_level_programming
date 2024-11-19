8d92a2ada25e465aae70a7c0a781f30679a72b14
Darya Javadi <darya.javadi15@gmail.com>
2024-11-19 13:51:10 +0000

    malloc2

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+/**
+ * array_range - array of integers
+ * @min: min
+ * @max: max
+ * Return: array
+ */
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i = 0;
+	if (min > max)
+		return (NULL);
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < max - min + 1; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
