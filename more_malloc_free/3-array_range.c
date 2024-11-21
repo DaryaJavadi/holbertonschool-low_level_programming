a76ddb7e50733a919fcb4bfbe81d2b738bb8d3a9
Darya Javadi <darya.javadi15@gmail.com>
2024-11-21 06:24:26 +0000

    malloc2

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,13 +11,11 @@
 int *array_range(int min, int max)
 {
 	int *arr, i;
-
 	if (min > max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int)*(max - min + 1));
-	
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 	{
 		return (NULL);
