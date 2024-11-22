f2cf178fdb2c0f033cb49296c0a22b0f28e5041d
Darya Javadi <darya.javadi15@gmail.com>
2024-11-22 11:25:48 +0000

    dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,49 +11,41 @@
 #include <string.h>
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
+	unsigned int nl, ol, i;
+	dog_t *d;
 
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
-	if (name == NULL)
-	{
-		free(p);
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
 		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		return (NULL);
-	}
+	for (nl = 0; name[nl]; nl++)
+	;
+	nl++;
 
-	int i = 0;
-	while (name[i] != '\0')
-	i++;
-	p->name = malloc(i + 1);
-	if (p->name == NULL)
+	d->name = malloc(nl * sizeof(char));
+	if (d->name == NULL)
 	{
-	free(p);
-	return (NULL);
-	}
-
-	for (int j = 0; j <= i; j++)
-	p->name[j] = name[j];
-
-	i = 0;
-	while (owner[i] != '\0')
-	i++;
-	p->owner = malloc(i + 1)
-	if (p->owner == NULL)
-	{
-	free(p->name);
-		free(p);
+		free(d);
 		return (NULL);
 	}
+	for (i = 0; i < nl; i++)
+		d->name[i] = name[i];
 
-	for (int j = 0; j <= i; j++)
-	p->owner[j] = owner[j];
-	p->age = age;
+	d->age = age;
+	for (ol = 0; owner[ol]; ol++)
+	;
+	ol++;
 
-	return (p);
+	d->owner = malloc(ol * sizeof(char));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		d->owner[i] = owner[i];
+	return (d);
 }
