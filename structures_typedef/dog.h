332ea72b7568999402d63a8259755edeb450cee6
Darya Javadi <darya.javadi15@gmail.com>
2024-11-22 10:07:02 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - creates dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of owner
+ *
+ * Description: struct of dog table
+ */
 struct dog
 {
 	char *name;
