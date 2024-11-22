c822f5717c3769ae6c29928a46c3635810941c80
Darya Javadi <darya.javadi15@gmail.com>
2024-11-22 11:23:48 +0000

    dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,18 +19,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL)
 	{
 		free(p);
-		free(owner);
 		return (NULL);
 	}
 	if (owner == NULL)
 	{
 		free(p);
-		free(name);
 		return (NULL);
 	}
 
-	p->name = name;
+	int i = 0;
+	while (name[i] != '\0')
+	i++;
+	p->name = malloc(i + 1);
+	if (p->name == NULL)
+	{
+	free(p);
+	return (NULL);
+	}
+
+	for (int j = 0; j <= i; j++)
+	p->name[j] = name[j];
+
+	i = 0;
+	while (owner[i] != '\0')
+	i++;
+	p->owner = malloc(i + 1)
+	if (p->owner == NULL)
+	{
+	free(p->name);
+		free(p);
+		return (NULL);
+	}
+
+	for (int j = 0; j <= i; j++)
+	p->owner[j] = owner[j];
 	p->age = age;
-	p->owner = owner;
+
 	return (p);
 }
