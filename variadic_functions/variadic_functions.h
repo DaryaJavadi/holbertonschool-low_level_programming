2502ff62c0c19e03879d279fb0b411e0105f9f95
Darya Javadi <darya.javadi15@gmail.com>
2024-11-22 13:06:15 +0000

    variadic

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,7 +5,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+#include <stdarg.h>
 void print_char(va_list args);
 void print_int(va_list args);
 void print_float(va_list args);
